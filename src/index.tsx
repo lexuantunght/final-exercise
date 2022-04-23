0569445300abbccb41e0e3e7263bea437670dd60
Xuan Tung Le <xuantung@Xuans-MacBook-Pro.local>
2022-04-23 15:35:16 +0700

    revert webpack

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
 import './utils/i18n';
 import App from './App';
+import './index.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
-import './index.css';
 import { Provider } from 'react-redux';
 import store from './utils/redux/store';
 
@@ -15,5 +14,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-reportWebVitals();
